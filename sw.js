7399416199d791eadbccea80b156b607b2059000
valentin5931 <valentin.wattelet@gmail.com>
2026-07-26 19:02:15 +0000

    Ajout de sw.js

---
+++ sw.js
@@ -0,0 +1,25 @@
+"use strict";
+const CACHE = "pensees-v1";
+const ASSETS = ["./", "./index.html", "./manifest.webmanifest",
+  "./icon-192.png", "./icon-512.png", "./apple-touch-icon.png"];
+
+self.addEventListener("install", e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)).then(() => self.skipWaiting()));
+});
+self.addEventListener("activate", e => {
+  e.waitUntil(caches.keys()
+    .then(ks => Promise.all(ks.filter(k => k !== CACHE).map(k => caches.delete(k))))
+    .then(() => self.clients.claim()));
+});
+self.addEventListener("fetch", e => {
+  if (e.request.method !== "GET") return;
+  e.respondWith(
+    caches.match(e.request, { ignoreSearch: true }).then(hit => hit ||
+      fetch(e.request).then(rep => {
+        const copie = rep.clone();
+        caches.open(CACHE).then(c => c.put(e.request, copie));
+        return rep;
+      }).catch(() => e.request.mode === "navigate" ? caches.match("./index.html") : undefined)
+    )
+  );
+});
